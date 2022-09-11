cd27bcf7e5bf30fe493479df278090e689e11f1e
husejinbg <48764304+husejinbg@users.noreply.github.com>
2022-09-11 10:05:04 +0300

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -8,4 +8,5 @@ apt install sublime-text
 
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | apt-key add -
 add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+apt update
 apt install code
